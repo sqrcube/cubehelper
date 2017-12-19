ef07d7f69ce40f52bba089053107e00f0d8bad17
sqrcube <34315933+sqrcube@users.noreply.github.com>
2017-12-19 19:13:06 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,64 @@
+var linkIMG = "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=250&pzl=3&view=plan&bg=t&fd=uuuuuuuuunnnnrrrrrnnnnfffffdddddddddnnnnlllllnnnnbbbbb&alg=";
+var oll = ["R U2 R2' F R F' U2 R' F R F'", 
+"F R U R' U' F' f R U R' U' f'", 
+"y' f R U R' U' f' U' F R U R' U' F'", 
+"y' f R U R' U' f' U F R U R' U' F'", 
+"r' U2 R U R' U r"];
+function newCase(){
+        var chosenCase = oll[Math.floor((Math.random() * 5))];
+        scrambleText.innerHTML = chosenCase;
+	document.getElementById("cubeImg").src= linkIMG + chosenCase;
+}
+var isOn;
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var timer = document.getElementById("timer");
+var toggleBtn = document.getElementById("toggle");
+var resetBtn = document.getElementById("reset");
+var saveBtn = document.getElementById("save");
+var showBtn = document.getElementById("show");
+var showingS = false;
+
+toggleBtn.addEventListener("click", function(){
+	if (isOn){
+		stop();
+		save();
+	        //getTimes();
+		//generateScramble();
+		toggle.innerHTML = "start timer";
+		newCase()
+	} else{
+	        timer.innerHTML = "0.000";
+		time = 0;
+		start();
+		toggle.innerHTML = "stop";
+	}
+});
+
+while(isOn){
+	update();
+}
+
+/*showBtn.addEventListener("click", function(){
+	if(showingS === false){
+		scrambleOutput.style.display = "inline";
+		console.log("hi" + showingS)
+		showingS = true;
+	} else{
+		scrambleOutput.style.display = "none";
+		showingS = false;
+	}
+	
+});*/
+
+/*resetBtn.addEventListener("click", function(){
+	timer.innerHTML = "0.000";
+	time = 0;
+});*/
+
+/*saveBtn.addEventListener("click", function(){
+	save();
+	getTimes();
+});*/
